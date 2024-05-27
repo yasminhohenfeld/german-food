75404dbe2f31552f5651c04d3a6d1748b6bcd890
yasminhohenfeld <ych.ascom@gmail.com>
2024-05-26 21:25:29 -0300

    update contactform

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -7,7 +7,60 @@ const ContactForm = () =>{
         message: '',
     });
 
-    
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //implementar a lógica aqui posteriormente para enviar msg        
+        console.log ('Mensagem enviada:', formData);
+    };
+
+    return (
+        <div className="container">
+            <h2>Contato</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="name">Nome</label>
+                    <input 
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    OnChange={handleChange}
+                    required
+                />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="message">Mensagem</label>
+                    <textarea
+                    className="form-control"
+                    id="message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChance}
+                    required
+                    ></textarea>
+                </div>
+                <button type= "submit" className="btn btn-primary">Enviar</button>
+            </form>
+        </div>
+    );
 };
+
 export default ContactForm;
